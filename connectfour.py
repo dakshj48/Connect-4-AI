b460324f4d94ea54af23b22234051945d57a7076
DJGamer48 <dakshjain48@gmail.com>
2019-03-13 01:58:16 -0400

    completed the board class

--- connectfour.py
+++ connectfour.py
@@ -4,12 +4,12 @@ class ConnectFourBoard:  # X is 0 and O is 1#
     parent = None
     move = None
 
-    def __init__(self, lst, board = None):
+    def __init__(self, lst, board=None):
         if board is None:
             self.parent = None
             self.map = {}
             for x in range(0, 42):
-                map[x] = lst[42-x]
+                self.map[x] = lst[42-x]
             self.turn = self.get_turn(lst)
         else:
             self.map = {}
@@ -19,7 +19,7 @@ class ConnectFourBoard:  # X is 0 and O is 1#
             else:
                 self.turn = 0
 
-    def get_turn(self, lst):
+    def get_turn(self, lst: [int]) -> int:
         x = 0
         o = 0
         for i in lst:
@@ -34,7 +34,7 @@ class ConnectFourBoard:  # X is 0 and O is 1#
         else:
             return 0
 
-    def get_Utility(self):
+    def get_utility(self):
         util = 0
         row = self.check_row()
         col = self.check_col()
@@ -51,13 +51,13 @@ class ConnectFourBoard:  # X is 0 and O is 1#
         i = 0
         while i < 39:
             val = self.map.get(i)
-            for x in range(1,4):
+            for x in range(1, 4):
                 if val != self.map.get(i+x):
                     break
                 if x == 3:
                     ret[val] = True
                     return ret
-            if i%7 == 3
+            if i % 7 == 3:
                 i += 3
             i += 1
         return ret
@@ -111,7 +111,7 @@ class ConnectFourBoard:  # X is 0 and O is 1#
                 i = 17
         return ret
 
-    def check_tie(self):
+    def check_tie(self, lst):
         for x in lst:
             if x == -1:
                 return False
@@ -129,13 +129,76 @@ class ConnectFourBoard:  # X is 0 and O is 1#
         return True
 
     def min_value(self):
+        if self.terminal_test():
+            return self.get_utility()
+        val = 10000
+        lst = self.next_states()
+        if not lst:
+            for x in lst:
+                comp = x.max_value()
+                if val > comp:
+                    val = comp
+        return val
 
     def max_value(self):
+        if self.terminal_test():
+            return self.get_utility()
+        val = -10000
+        lst = self.next_states()
+        if not lst:
+            for x in lst:
+                comp = x.min_value()
+                if val < comp:
+                    val = comp
+        return val
 
     def minimax_decision(self):
+        val = 0
+        if self.turn:
+            val = self.min_value()
+        else:
+            val = self.max_value()
+        if val != 1 and val != -1:
+            val = 0
+        return val
 
-    def print_move(self):
+    def print_move(self, max_val):
+        lst = self.next_states()
+        for x in lst:
+            com: int = 0
+            if self.turn:
+                com = x.max_value()
+            else:
+                com = x.min_value()
+            if com != 1 and com != -1:
+                com = 0
+            if com == max_val:
+                if self.turn:
+                    print("Utility: " + max_val + " O" + x.move)
+                else:
+                    print("Utility: " + max_val + " X" + x.move)
+            break
 
     def next_states(self):
+        ret = []
+        if not self.terminal_test():
+            for x in range(0, 7):
+                for y in range(0, 6):
+                    if self.map.get(x+7*y) == -1:
+                        to_add = ConnectFourBoard(self)
+                        to_add.move = x
+                        to_add.map[x+7*y] = to_add.parent.turn
+                        ret.append(to_add)
+                        break
+        return ret
 
     def terminal_test(self):
+        row = self.check_row()
+        col = self.check_col()
+        diag = self.check_diag()
+        antidiag = self.check_antidiag()
+        if row.get(0) or col.get(0) or diag.get(0) or antidiag.get(0):
+            return True
+        elif row.get(1) or col.get(1) or diag.get(1) or antidiag.get(1):
+            return True
+        return False
